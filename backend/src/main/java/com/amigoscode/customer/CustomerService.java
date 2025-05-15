993261b11b2866d4c632f37fb2ebcc10215995c0
PSBrar <prabhjot.brarrr@gmail.com>
2025-05-15 14:04:21 +0100

    Added customer profile pic upload/download controller paths + db field for profile pic

--- backend/src/main/java/com/amigoscode/customer/CustomerService.java
+++ backend/src/main/java/com/amigoscode/customer/CustomerService.java
@@ -3,11 +3,16 @@ package com.amigoscode.customer;
 import com.amigoscode.exception.DuplicateResourceException;
 import com.amigoscode.exception.RequestValidationException;
 import com.amigoscode.exception.ResourceNotFoundException;
+import com.amigoscode.s3.S3Buckets;
+import com.amigoscode.s3.S3Service;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 @Service
@@ -16,13 +21,17 @@ public class CustomerService {
     private final CustomerDao customerDao;
     private final CustomerDTOMapper customerDTOMapper;
     private final PasswordEncoder passwordEncoder;
+    private final S3Service s3Service;
+    private final S3Buckets s3Buckets;
 
     public CustomerService(@Qualifier("jdbc") CustomerDao customerDao,
                            CustomerDTOMapper customerDTOMapper,
-                           PasswordEncoder passwordEncoder) {
+                           PasswordEncoder passwordEncoder, S3Service s3Service, S3Buckets s3Buckets) {
         this.customerDao = customerDao;
         this.customerDTOMapper = customerDTOMapper;
         this.passwordEncoder = passwordEncoder;
+        this.s3Service = s3Service;
+        this.s3Buckets = s3Buckets;
     }
 
     public List<CustomerDTO> getAllCustomers() {
@@ -61,13 +70,17 @@ public class CustomerService {
     }
 
     public void deleteCustomerById(Integer customerId) {
+        checkIfCustomerExistsOrThrow(customerId);
+
+        customerDao.deleteCustomerById(customerId);
+    }
+
+    private void checkIfCustomerExistsOrThrow(Integer customerId) {
         if (!customerDao.existsCustomerById(customerId)) {
             throw new ResourceNotFoundException(
                     "customer with id [%s] not found".formatted(customerId)
             );
         }
-
-        customerDao.deleteCustomerById(customerId);
     }
 
     public void updateCustomer(Integer customerId,
@@ -105,6 +118,34 @@ public class CustomerService {
         }
 
         customerDao.updateCustomer(customer);
+    }
+
+    public void uploadCustomerImage(Integer customerId, MultipartFile file){
+        checkIfCustomerExistsOrThrow(customerId);
+        String imageId = UUID.randomUUID().toString();
+
+        try {
+            s3Service.putObject(s3Buckets.getCustomer(),
+                    "profile-images/%s/%s".formatted(customerId, imageId),
+                    file.getBytes());
+        }
+        catch(IOException e){
+            throw new RuntimeException("customer image upload failed", e);
+        }
+        //TODO: Store imageId to postgres
+    }
+
+    public byte[] getProfileImage(Integer customerId) {
+        CustomerDTO customer = getCustomer(customerId);
+
+        //TODO: Check if imageId is empty or null
+
+        byte[] profileImage = s3Service.getObject(
+                s3Buckets.getCustomer(),
+                "profile-images/%s/%s".formatted(customerId, "TODO")
+        );
+
+        return profileImage;
     }
 }
 
